a8bcf42a748ad5e104a3c0db4eb64b8f2adb910c
Jasem Jama <Jas2256355@maricopa.edu>
2024-04-19 15:28:45 -0700

    Snake Game

--- Snake/SnakeGame.java
+++ Snake/SnakeGame.java
@@ -1,7 +1,18 @@
 package Snake;
 
-public abstract class SnakeGame {
-    public static void main(String[] args){
-  new GameFrame();
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+public class SnakeGame {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            JFrame frame = new JFrame("Snake");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setResizable(false);
+            frame.add(new GamePanel());
+            frame.pack();
+            frame.setLocationRelativeTo(null);
+            frame.setVisible(true);
+        });
     }
 }
